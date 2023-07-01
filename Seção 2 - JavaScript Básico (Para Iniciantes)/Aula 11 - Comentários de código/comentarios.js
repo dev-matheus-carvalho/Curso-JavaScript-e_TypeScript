d1e7ca4dacdced0fc8465cd05d0606a4f41755a3
Matheus Carvalho <devmatheuscarvalho@gmail.com>
2023-07-01 13:26:15 -0300

    Aprendendo sobre comentários

---
+++ Seção 2 - JavaScript Básico (Para Iniciantes)/Aula 11 - Comentários de código/comentarios.js
@@ -0,0 +1,9 @@
+// Isto é um comentário no qual será ingnorado
+
+console.log('Comentário simples anterior foi ignorado');
+
+/* Este é outro comentário
+porém com quebra de linhas
+*/
+
+console.log('Comentário de várias linhas anterior foi ignorado');
