481234f3cf7b7d74b282656ad05c6a91242fc982
Matheus Carvalho <devmatheuscarvalho@gmail.com>
2023-07-04 16:27:47 -0300

    Aprendendo sobre tipos de dados primitivos

---
+++ Seção 2 - JavaScript Básico (Para Iniciantes)/Aula 19 - Tipos de dados primitivos/index.js
@@ -0,0 +1,37 @@
+// String
+
+const nome = 'Matheus';
+const nome1 = "Matheus";
+const nome2 = `Matheus`;
+
+// Number
+
+const num = 10;
+const num1 = 10.52;
+
+// Undefined
+
+let nomeAluno;
+
+// Null
+
+let sobrenomeAluno = null;
+
+/*
+
+OBS: Tanto quanto undefined e null não apontam para local nehuma da memória, porém o null é usado para desconfigurar uma variável.
+
+*/
+
+// Booleano
+
+const booleano = true;
+
+// Teste
+
+let a = 2;
+const b = a;
+console.log(a, b); // 2 2
+
+a = 3;
+console.log(a, b); // 3 2
