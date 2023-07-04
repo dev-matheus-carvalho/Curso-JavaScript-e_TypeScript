1a215694e3634608e692b1468cbde292d1eed00e
Matheus Carvalho <devmatheuscarvalho@gmail.com>
2023-07-04 15:24:54 -0300

    Exercício de const e let concluído

---
+++ Seção 2 - JavaScript Básico (Para Iniciantes)/Aula 16 - Exercícios (Const e Let)/index.js
@@ -0,0 +1,11 @@
+const nome = 'Matheus Luan';
+const sobrenome = 'Campos de Carvalho';
+const altura = 1.65;
+let idade = 26;
+let peso = 62;
+
+const imc = peso / (altura * altura);
+
+//console.log(nome, sobrenome, 'tem', idade, 'anos, pesa', peso, 'Kg e tem', altura, 'de altura. Seu IMC é de', imc);
+
+console.log(`${nome} ${sobrenome} tem ${idade} anos, pesa ${peso} Kg e tem ${altura} de altura. Seu IMC é de ${imc}`);
