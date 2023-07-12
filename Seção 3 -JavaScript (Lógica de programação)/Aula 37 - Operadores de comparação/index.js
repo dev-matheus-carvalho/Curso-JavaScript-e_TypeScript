dacf58212bf16cad656db88562f22f958eb33d66
Matheus Carvalho <devmatheuscarvalho@gmail.com>
2023-07-12 09:13:16 -0300

    Aula sobre operadores de comparação

---
+++ Seção 3 -JavaScript (Lógica de programação)/Aula 37 - Operadores de comparação/index.js
@@ -0,0 +1,32 @@
+/*
+
+Operadores de comparação
+
+<   Menor
+<=  Menor que
+
+>   Maior
+>=  Maior que
+
+==  Igualdade
+=== Igualdade estrita
+
+!=  Diferente
+!== Diferente estrito
+
+*/
+
+const comp = 10 > 5;
+console.log(comp);
+
+const comp1 = 10 >= 5;
+console.log(comp1);
+
+const comp2 = 10 === 11; // 10 é igual a 11? false
+console.log(comp2);
+
+const comp3 = 10 !== 11; 
+console.log(comp3);
+
+
+
