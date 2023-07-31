a88092714431f42dcebff055a4acb86ec878795b
Matheus Carvalho <devmatheuscarvalho@gmail.com>
2023-07-31 16:14:10 -0300

    Exercício de criar um timer usando DOM

---
+++ Seção 3 -JavaScript (Lógica de programação)/Aula 66 - Exercício (Criando um timer com setInterval)/assets/js/main.js
@@ -0,0 +1,46 @@
+function criaHoraDosSegundos(segundos){
+    const data = new Date(segundos * 1000);
+    return data.toLocaleTimeString('pt-BR', {
+        hour12: false,
+        timeZone: 'UTC'
+    });
+}
+
+
+const relogio = document.querySelector('.relogio');
+const iniciar = document.querySelector('.iniciar');
+const pausar = document.querySelector('.pausar');
+const zerar = document.querySelector('.zerar');
+
+let segundos = 0;
+let timer;
+
+function iniciaRelogio (){
+    timer = setInterval(function() {
+        segundos++;
+        relogio.innerHTML = criaHoraDosSegundos(segundos);
+    }, 1000);
+}
+
+document.addEventListener('click', function(event){
+    const element = event.target;
+
+    if(element.classList.contains('zerar')){
+        relogio.classList.remove('pausado');
+        clearInterval(timer);
+        relogio.innerHTML = '00:00:00';
+        segundos = 0;
+    }
+
+    if(element.classList.contains('pausar')){
+        relogio.classList.add('pausado');
+        clearInterval(timer);
+    }
+
+    if(element.classList.contains('iniciar')){
+        relogio.classList.remove('pausado');
+        clearInterval(timer);
+        iniciaRelogio();
+    }
+});
+
