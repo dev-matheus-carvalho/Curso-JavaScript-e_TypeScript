4706e258207272d4995ff4176a720b0d6a7be7d5
Matheus Carvalho <devmatheuscarvalho@gmail.com>
2023-08-02 15:50:22 -0300

    Aprendendo sobre funções imediatas

---
+++ Seção 4 - JavaScript Funções (Avançado)/Aula 74 - Funções imediatas (IIFE)/index.js
@@ -0,0 +1,11 @@
+// IIFE -> Immediately Invoked Function Expresssion
+
+(function(){
+    const nome = 'Matheus';
+
+    function falaOi(nome){
+        console.log(`Oi ${nome}`);
+    }
+
+    falaOi(nome);
+})();
