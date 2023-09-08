06b56445413df58d1a7a3d8d45ff8dac906ba06c
Matheus Carvalho <devmatheuscarvalho@gmail.com>
2023-09-08 16:32:54 -0300

    Introdução ao Express

---
+++ Seção 10 - JavaScript (Node, Npm, Express e MongoDB [NodeJs])/Aula 130 - Express (Introdução)/node/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+
+//Get
+app.get('/', (req, res) => {
+    res.send(`
+    <form action="/" method="POST">
+        Nome: <input type="text" name="nome">
+        <button>Enviar</button>
+    </form>
+    `);
+});
+
+app.post('/', (req, res) => {
+    res.send('Recebi o formulário');
+});
+
+app.get('/contato', (req, res) => {
+    res.send('Contato');
+});
+
+app.listen(3000, () => {
+    console.log('http://localhost:3000');
+    console.log('Servidor rodando na porta 3000');
+});
