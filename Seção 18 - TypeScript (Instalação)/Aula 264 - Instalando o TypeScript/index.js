4e1a2cb3eb0576a4596f516672d14d9190f97cc4
Matheus Carvalho <devmatheuscarvalho@gmail.com>
2023-09-14 10:00:23 -0300

    Instalando TS e fazendo os primeiros códigos

---
+++ Seção 18 - TypeScript (Instalação)/Aula 264 - Instalando o TypeScript/index.js
@@ -0,0 +1,6 @@
+// function greet(name) {
+//     return "Ol\u00E1 ".concat(name);
+// }
+// console.log(greet('Matheus Carvalho'));
+
+const nome = 'Matheus Carvalho';
---
+++ Seção 18 - TypeScript (Instalação)/Aula 264 - Instalando o TypeScript/index.ts
@@ -0,0 +1,9 @@
+// function greet(name: string) {
+//     return `Olá ${name}`;
+// }
+
+// console.log(greet('Matheus Carvalho'));
+
+const nome: string = 'Matheus Carvalho';
+
+export default nome;
