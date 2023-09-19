0823f70b5bef46d8cbfce6193388d35e1f29ee06
Matheus Carvalho <devmatheuscarvalho@gmail.com>
2023-09-19 09:15:34 -0300

    Explicação sobre injeção de dependecia

---
+++ Seção 20 - TypeScript (Classes e Interfaces [POO])/Aula 304 - Inversão de dependências (Dependency Inversion)/src/index.ts
@@ -0,0 +1,59 @@
+export class Escritor {
+  private _ferramenta: Ferramenta | null = null;
+
+  constructor(private _nome: string) {}
+
+  get nome(): string {
+    return this._nome;
+  }
+
+  set ferramenta(ferramenta: Ferramenta | null) {
+    this._ferramenta = ferramenta;
+  }
+
+  get ferramenta(): Ferramenta | null {
+    return this._ferramenta;
+  }
+
+  escrever(): void {
+    if (this._ferramenta === null) {
+      console.log('Não posso escrever sem ferramenta');
+      return;
+    }
+    this.ferramenta?.escrever();
+  }
+}
+
+export abstract class Ferramenta {
+  constructor(private _nome: string) {}
+  abstract escrever(): void;
+
+  get nome(): string {
+    return this._nome;
+  }
+}
+
+export class Caneta extends Ferramenta {
+  escrever(): void {
+    console.log(`${this.escrever} está escrevendo...`);
+  }
+}
+
+export class Maquina extends Ferramenta {
+  escrever(): void {
+    console.log(`${this.escrever} está digitando...`);
+  }
+}
+
+const escritor = new Escritor('Matheus');
+const caneta = new Caneta('Bic');
+const maquina = new Maquina('Imperial Typewriter');
+
+console.log(escritor.nome);
+console.log(caneta.nome);
+console.log(maquina.nome);
+
+escritor.ferramenta = caneta;
+escritor.ferramenta = maquina;
+escritor.ferramenta = null;
+escritor.escrever();
